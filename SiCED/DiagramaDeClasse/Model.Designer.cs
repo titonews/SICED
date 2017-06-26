542764249c8cc1fe8d2c93e8896c714a2d061db6
Alexsandro Moreira <titonews@hotmail.com>
2017-06-26 18:41:06 -0400

    Apenas organizei o diagrama para print

--- SiCED/DiagramaDeClasse/Model.Designer.cs
+++ SiCED/DiagramaDeClasse/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Alexsandro Moreira\Documents\Visual Studio 2017\Projects\SiCED\SiCED\DiagramaDeClasse\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Alexsandro Moreira\Source\Repos\SICED\SiCED\DiagramaDeClasse\Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
